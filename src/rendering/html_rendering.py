644f45a87d190a9992e136e765197f86c4fd32b4
Michele Schimd <michele.schimd@itiszuccante.edu.it>
2023-02-24 10:41:06 +0100

    Choice question renders in HTML.

--- src/rendering/html_rendering.py
+++ src/rendering/html_rendering.py
@@ -1,18 +1,53 @@
 import html
 
-fill_placeholder = ".................."
+fill_placeholder ='<input type="textbox" style="padding: 1px">'
 
 def question_header(i):
     return f'<h2>Domanda {i}</h2>\n'
 
 def html_render_choices(q):
-    pass
+    content_text = ''
+    content_solution = ''
+    text = q._text
+    options = q._options
+    correct = q._correct
+	
+    content_text += f'<p>{text}</p>\n'
+    content_solution += f'<p>{text}</p>\n'
+    
+    for j in range(len(options)):
+        opt = f'<p class="option"><input type="checkbox" id="ans-{j}">\n<label for="ans-{j}">{options[j]}</label></p>'
+        content_text += opt
+        checked = 'checked' if correct[j] == 1 else ''
+        opt = f'<p class="option"><input type="checkbox" id="ans-{j}" disabled {checked}>\n<label for="ans-{j}">{options[j]}</label></p>'
+        content_solution += opt
+        j += 1
+    content_text += '\n'
+    content_solution += '\n'
+    
+    return content_text, content_solution
 
 def html_render_open(q):
-    pass
+    content_text = q._text
+    content_solution = q._text
+    return content_text, content_solution
     
 def html_render_fill(q):
-    pass
+    content_text = ''
+    content_solution = ''
+    correct = q._correct
+    content_text += f'<p>{q._text}</p>\n\n'
+    content_solution += f'<p>{q._text}</p>\n\n'
+    to_fill = q._to_fill
+    sol_filled = to_fill
+    for j in range(len(correct)):
+        sol_filled = sol_filled.replace(f'{{{{{j}}}}}', correct[j].upper())
+        to_fill = to_fill.replace(f'{{{{{j}}}}}', fill_placeholder)
+
+    content_text += f'<p>{to_fill}</p>\n\n'
+    content_solution += f'<p>{sol_filled}</p>\n\n'
+
+    return content_text, content_solution
 
 def html_render_exercise(q):
     content_text = q._text
